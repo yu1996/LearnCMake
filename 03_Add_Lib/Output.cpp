319b632229321ba0448a14f1bd986b9baa1e657d
yu <394771449@qq.com>
2019-03-19 15:48:44 +0800

    add lib
    add lib

---
+++ 03_Add_Lib/Output.cpp
@@ -0,0 +1,7 @@
+#include "Output.hpp"
+#include <iostream>
+
+void COutput::outputStr()
+{
+	std::cout << "hello cmake" << std::endl << "here is chapter 03";
+}
---
+++ 03_Add_Lib/Output.hpp
@@ -0,0 +1,6 @@
+#pragma once
+class COutput
+{
+public:
+	void outputStr();
+};
